92557602ace112471f46dbe4f6b5c2300e25a1a7
sebastian <Sebastian2205>
2019-03-20 12:46:04 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+//const DB = require("./db");
+
+const app = express();
+const port = process.env.PORT || 1345;
+
+/*
+const db = new DB();
+
+db.init()
+    .then(() => console.log("DB ready"))
+    .catch(err => console.log(err));
+*/
+
+app.listen(port, () => {
+    console.log("Server is listening on port " + port);
+
+})
+
+app.get("/", async (req, res) => {
+
+    //let user_object = await db.get_row("SELECT * FROM personal");
+    res.send("Hello World");
+});
